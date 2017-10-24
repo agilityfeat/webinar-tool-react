34f30d7547eea9d9a7c8b4ed75fb98ecd5b63fab
Hector Zelaya <inge.zelaya@gmail.com>
2017-10-24 17:17:18 -0600

    adding call capabilities

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -28,6 +28,8 @@ const credentials = {
 }
 const mainChannel = 'realtime-app';
 
+const number = ('' + Math.random() * 100000).split('.')[0];
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -53,6 +55,8 @@ class App extends React.Component {
             rateBoxStyle: null,
             index: 0,
             direction: null,
+            showCallingModal: false,
+            showConferenceModal: false
         }
         //bindings
         this.removeStyles = this.removeStyles.bind(this);
@@ -64,9 +68,12 @@ class App extends React.Component {
         this.sendRate = this.sendRate.bind(this);
         this.sendComment = this.sendComment.bind(this);
         this.sendDeleteComment = this.sendDeleteComment.bind(this);
+        this.callUser = this.callUser.bind(this);
+        this.hangUp = this.hangUp.bind(this);
     }
 
     componentDidMount() {
+        console.log(number);
         this.PubNub = PUBNUB.init({
             publish_key: credentials.publish_key,
             subscribe_key: credentials.subscribe_key
@@ -75,6 +82,38 @@ class App extends React.Component {
             channel: mainChannel,
             message: this.receiveMessages
         });
+        if (navigator.mediaDevices) {
+            this.phone = PHONE({
+                number: number,
+                publish_key: credentials.publish_key,
+                subscribe_key: credentials.subscribe_key
+            });
+            this.phone.camera.ready(video => {
+                console.log('Camera Ready');
+                this.localvideo = video;
+            });
+            this.phone.receive(session => {
+                console.log('receiving call from:', session);
+                session.connected(session => {
+                    console.log('Session: CONNECTED');
+
+                    session.video.className = 'you';
+                    session.video.width = this.localvideo.width;
+                    session.video.height = this.localvideo.height;
+                    this.localvideo.className = 'me';
+
+                    this.toggleModal('conference');
+                    if (this.state.isPresenter) this.toggleModal('calling');
+
+                    this.video.appendChild(this.localvideo);
+                    this.video.appendChild(session.video);
+                });
+                session.ended(session => {
+                    console.log('Session: ENDED');
+                    this.toggleModal('conference');
+                })
+            });
+        }
         window.addEventListener('resize', this.removeStyles);
     }
 
@@ -166,7 +205,7 @@ class App extends React.Component {
                     prevState.comments.push({
                         text: message.comment,
                         user: message.user,
-                        can_webrtc: navigator.mediaDevices.getUserMedia ? true : false
+                        can_webrtc: message.can_webrtc
                     });
                     return {
                         comments: prevState.comments
@@ -204,7 +243,8 @@ class App extends React.Component {
         this.sendMessage({
             type: 'COMMENT',
             comment: text,
-            user: 0
+            user: number,
+            can_webrtc: navigator.mediaDevices ? true : false
         })
     }
 
@@ -213,6 +253,27 @@ class App extends React.Component {
             type: 'DELETE',
             index: index
         });
+    }
+
+    toggleModal(modal) {
+        switch (modal) {
+            case 'calling':
+                this.setState({ showCallingModal: !this.state.showCallingModal });
+                break;
+            case 'conference':
+                this.setState({ showConferenceModal: !this.state.showConferenceModal });
+                break;
+        }
+    }
+
+    callUser(number) {
+        console.log('calling ' + number + '...')
+        this.toggleModal('calling');
+        let session = this.phone.dial(number);
+    }
+
+    hangUp() {
+        this.phone.hangup();
     }
 
     //the render method
@@ -236,8 +297,9 @@ class App extends React.Component {
                     <Comments isPresenter={this.state.isPresenter}
                         style={this.state.commentsStyle}
                         onToggle={this.togglePlayerComments}
-                        comments={this.state.comments} 
-                        onDeleteComment={this.sendDeleteComment} />
+                        comments={this.state.comments}
+                        onDeleteComment={this.sendDeleteComment}
+                        onCall={this.callUser} />
                 </Row>
 
                 <Row>
@@ -247,9 +309,36 @@ class App extends React.Component {
                         rateBoxStyle={this.state.rateBoxStyle}
                         onToggleCommentBox={this.toggleCommentBox}
                         presentationVotes={this.state.presentationVotes}
-                        onRate={this.sendRate} 
+                        onRate={this.sendRate}
                         onAddComment={this.sendComment} />
                 </Row>
+
+                <Modal show={this.state.showCallingModal} onHide={() => this.toggleModal('calling')}>
+                    <Modal.Header closeButton>
+                        <Modal.Title><img src={pathToImages("./phone.svg", true)} className="shake" /> Outgoing Call</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p className="calling">Calling (User)...</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={() => this.toggleModal('calling')}>Cancel</Button>
+                    </Modal.Footer>
+                </Modal>
+
+                <Modal show={this.state.showConferenceModal} onHide={this.hangUp}>
+                    <Modal.Header closeButton>
+                        <Modal.Title><img className="phone_icon" src={pathToImages("./phone.svg", true)} /> Person to Person Call</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div ref={input => this.video = input}></div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <div className="span12" id="video-controls">
+                            <Button onClick={this.hangUp}>Hang Up</Button>
+                            <span id="time"><Clock /></span>
+                        </div>
+                    </Modal.Footer>
+                </Modal>
             </div>
         );
     }
